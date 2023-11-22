29644948ef58ccb923193730d0d19863b5aaaab5
Xcilik <149920327+Xcilik@users.noreply.github.com>
2023-11-22 19:43:18 +0700

    Update karma.py

--- wbb/modules/karma.py
+++ wbb/modules/karma.py
@@ -43,12 +43,11 @@ from wbb.utils.dbfunctions import (
 from wbb.utils.filter_groups import karma_negative_group, karma_positive_group
 from wbb.utils.functions import get_user_id_and_usernames
 
-__MODULE__ = "Karma"
-__HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks", etc to upvote a message.
-[DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
-/karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
-Reply to a message with /karma to check a user's karma
-Send /karma without replying to any message to check karma list of top 10 users"""
+__MODULE__ = "OpenAI"
+__HELP__ = """
+/ai [question] - Generated text / ChatGpt
+/dalle [text] - Generated Image
+"""
 
 regex_upvote = r"^(\++|\+1|thx|tnx|tq|ty|thankyou|thank you|thanx|thanks|pro|cool|good|agree|👍|\++ .+)$"
 regex_downvote = r"^(-+|-1|not cool|disagree|worst|bad|👎|-+ .+)$"
