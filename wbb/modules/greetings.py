e229b503e7a44a9bb3d714c7c97d63b0a229cbd1
Xcilik <149920327+Xcilik@users.noreply.github.com>
2023-11-22 17:04:13 +0700

    Update greetings.py

--- wbb/modules/greetings.py
+++ wbb/modules/greetings.py
@@ -67,8 +67,8 @@ from wbb.utils.functions import (
     generate_captcha,
 )
 
-
-"""
+__MODULE__ = "Greetings"
+__HELP__ = """
 /captcha [ENABLE|DISABLE] - Enable/Disable captcha.
 
 /set_welcome - Reply this to a message containing correct
@@ -104,3 +104,275 @@ loop = asyncio.get_running_loop()
 
 
 
+    # Mute new member and send message with button
+
+
+async def send_welcome_message(chat: Chat, user_id: int, delete: bool = False):
+    welcome, raw_text, file_id = await get_welcome(chat.id)
+
+    if not raw_text:
+        return
+    text = raw_text
+    keyb = None
+    if "~" in raw_text:
+        text, keyb = extract_text_and_keyb(ikb, raw_text)
+
+    if "{chat}" in text:
+        text = text.replace("{chat}", chat.title)
+    if "{name}" in text:
+        text = text.replace("{name}", (await app.get_users(user_id)).mention)
+
+    async def _send_wait_delete():
+        if welcome == "Text":
+            m = await app.send_message(
+                chat.id,
+                text=text,
+                reply_markup=keyb,
+                disable_web_page_preview=True,
+            )
+        elif welcome == "Photo":
+            m = await app.send_photo(
+                chat.id,
+                photo=file_id,
+                caption=text,
+                reply_markup=keyb,
+            )
+        else:
+            m = await app.send_animation(
+                chat.id,
+                animation=file_id,
+                caption=text,
+                reply_markup=keyb,
+            )
+        await asyncio.sleep(300)
+        await m.delete()
+
+    asyncio.create_task(_send_wait_delete())
+
+
+@app.on_callback_query(filters.regex("pressed_button"))
+async def callback_query_welcome_button(_, callback_query):
+    """After the new member presses the correct button,
+    set his permissions to chat permissions,
+    delete button message and join message.
+    """
+    global answers_dicc
+    data = callback_query.data
+    pressed_user_id = callback_query.from_user.id
+    pending_user_id = int(data.split(None, 2)[2])
+    button_message = callback_query.message
+    answer = data.split(None, 2)[1]
+
+    correct_answer = None
+    keyboard = None
+
+    if len(answers_dicc) != 0:
+        for i in answers_dicc:
+            if (
+                i["user_id"] == pending_user_id
+                and i["chat_id"] == button_message.chat.id
+            ):
+                correct_answer = i["answer"]
+                keyboard = i["keyboard"]
+
+    if not (correct_answer and keyboard):
+        return await callback_query.answer(
+            "Something went wrong, Rejoin the " "chat!"
+        )
+
+    if pending_user_id != pressed_user_id:
+        return await callback_query.answer("This is not for you")
+
+    if answer != correct_answer:
+        await callback_query.answer("Yeah, It's Wrong.")
+        for iii in answers_dicc:
+            if (
+                iii["user_id"] == pending_user_id
+                and iii["chat_id"] == button_message.chat.id
+            ):
+                attempts = iii["attempts"]
+                if attempts >= 3:
+                    answers_dicc.remove(iii)
+                    await button_message.chat.ban_member(pending_user_id)
+                    await asyncio.sleep(1)
+                    await button_message.chat.unban_member(pending_user_id)
+                    await button_message.delete()
+                    return await update_captcha_cache(answers_dicc)
+
+                iii["attempts"] += 1
+                break
+
+        shuffle(keyboard[0])
+        shuffle(keyboard[1])
+        shuffle(keyboard[2])
+        shuffle(keyboard)
+        keyboard = InlineKeyboardMarkup(keyboard)
+        return await button_message.edit(
+            text=button_message.caption.markdown,
+            reply_markup=keyboard,
+        )
+
+    await callback_query.answer("Captcha passed successfully!")
+    await button_message.chat.unban_member(pending_user_id)
+    await button_message.delete()
+
+    if len(answers_dicc) != 0:
+        for ii in answers_dicc:
+            if (
+                ii["user_id"] == pending_user_id
+                and ii["chat_id"] == button_message.chat.id
+            ):
+                answers_dicc.remove(ii)
+                await update_captcha_cache(answers_dicc)
+
+    chat = callback_query.message.chat
+
+    # Save this verification in db, so we don't have to
+    # send captcha to this user when he joins again.
+    await save_captcha_solved(chat.id, pending_user_id)
+
+    return await send_welcome_message(chat, pending_user_id, True)
+
+
+async def kick_restricted_after_delay(
+    delay, button_message: Message, user: User
+):
+    """If the new member is still restricted after the delay, delete
+    button message and join message and then kick him
+    """
+    global answers_dicc
+    await asyncio.sleep(delay)
+    group_chat = button_message.chat
+    user_id = user.id
+    await button_message.delete()
+    if len(answers_dicc) != 0:
+        for i in answers_dicc:
+            if i["user_id"] == user_id:
+                answers_dicc.remove(i)
+                await update_captcha_cache(answers_dicc)
+    await _ban_restricted_user_until_date(group_chat, user_id, duration=delay)
+
+
+async def _ban_restricted_user_until_date(
+    group_chat, user_id: int, duration: int
+):
+    try:
+        member = await group_chat.get_member(user_id)
+        if member.status == ChatMemberStatus.RESTRICTED:
+            until_date = int(datetime.utcnow().timestamp() + duration)
+            await group_chat.ban_member(user_id, until_date=until_date)
+    except UserNotParticipant:
+        pass
+
+
+@app.on_message(filters.command("captcha") & ~filters.private)
+@adminsOnly("can_restrict_members")
+async def captcha_state(_, message):
+    usage = "**Usage:**\n/captcha [ENABLE|DISABLE]"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+
+    chat_id = message.chat.id
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await captcha_on(chat_id)
+        await message.reply_text("Enabled Captcha For New Users.")
+    elif state == "disable":
+        await captcha_off(chat_id)
+        await message.reply_text("Disabled Captcha For New Users.")
+    else:
+        await message.reply_text(usage)
+
+
+# WELCOME MESSAGE
+
+
+@app.on_message(filters.command("set_welcome") & ~filters.private)
+@adminsOnly("can_change_info")
+async def set_welcome_func(_, message):
+    usage = "You need to reply to a text, gif or photo to set it as greetings.\n\nNotes: caption required for gif and photo."
+    key = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="More Help",
+                    url=f"t.me/{BOT_USERNAME}?start=help_greetings",
+                )
+            ],
+        ]
+    )
+    replied_message = message.reply_to_message
+    chat_id = message.chat.id
+    try:
+        if not replied_message:
+            await message.reply_text(usage, reply_markup=key)
+            return
+        if replied_message.animation:
+            welcome = "Animation"
+            file_id = replied_message.animation.file_id
+            text = replied_message.caption
+            if not text:
+                return await message.reply_text(usage, reply_markup=key)
+            raw_text = text.markdown
+        if replied_message.photo:
+            welcome = "Photo"
+            file_id = replied_message.photo.file_id
+            text = replied_message.caption
+            if not text:
+                return await message.reply_text(usage, reply_markup=key)
+            raw_text = text.markdown
+        if replied_message.text:
+            welcome = "Text"
+            file_id = None
+            text = replied_message.text
+            raw_text = text.markdown
+        if replied_message.reply_markup and not "~" in raw_text:
+            urls = extract_urls(replied_message.reply_markup)
+            if urls:
+                response = "\n".join(
+                    [f"{name}=[{text}, {url}]" for name, text, url in urls]
+                )
+                raw_text = raw_text + response
+        raw_text = await check_format(ikb, raw_text)
+        if raw_text:
+            await set_welcome(chat_id, welcome, raw_text, file_id)
+            return await message.reply_text(
+                "Welcome message has been successfully set."
+            )
+        else:
+            return await message.reply_text(
+                "Wrong formatting, check the help section.\n\n**Usage:**\nText: `Text`\nText + Buttons: `Text ~ Buttons`",
+                reply_markup=key,
+            )
+    except UnboundLocalError:
+        return await message.reply_text(
+            "**Only Text, Gif and Photo welcome message are supported.**"
+        )
+
+
+@app.on_message(filters.command("del_welcome") & ~filters.private)
+@adminsOnly("can_change_info")
+async def del_welcome_func(_, message):
+    chat_id = message.chat.id
+    await del_welcome(chat_id)
+    await message.reply_text("Welcome message has been deleted.")
+
+
+@app.on_message(filters.command("get_welcome") & ~filters.private)
+@adminsOnly("can_change_info")
+async def get_welcome_func(_, message):
+    chat = message.chat
+    welcome, raw_text, file_id = await get_welcome(chat.id)
+    if not raw_text:
+        return await message.reply_text("No welcome message set.")
+    if not message.from_user:
+        return await message.reply_text(
+            "You're anon, can't send welcome message."
+        )
+
+    await send_welcome_message(chat, message.from_user.id)
+
+    await message.reply_text(
+        f'Welcome: {welcome}\n\nFile_id: `{file_id}`\n\n`{raw_text.replace("`", "")}`'
+        )
