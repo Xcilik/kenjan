da430ff47ff5f6e343c3a4a6e7ce6052711383c0
Xcilik <149920327+Xcilik@users.noreply.github.com>
2023-11-23 09:00:46 -0800

    Update karma.py

--- wbb/modules/karma.py
+++ wbb/modules/karma.py
@@ -26,6 +26,7 @@ import re
 from pyrogram import filters
 
 from wbb import app
+import os
 import requests
 import openai as anuan
 import g4f
@@ -128,8 +129,9 @@ async def iamges(_, message):
         try:            
             downloader.download(msg, limit=5, output_dir='downloads', adult_filter_off=True, force_replace=False)
             for i in range(1, 6):
-                image_path = f"downloads/{msg}/{msg}{i}.jpg"
+                image_path = f"downloads/{msg}/Image{i}.jpg"
                 await app.send_photo(message.chat.id, photo=open(image_path, 'rb'))
+                os.rmtree(image_path)
             await cilik.delete()
         except Exception as e:
             await cilik.edit(f"{e}")
