35b049026cbdeae9b604b309d2adf8cdc3d99ace
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-02-19 21:43:05 +0700

    Update crypto.py

--- wbb/modules/crypto.py
+++ wbb/modules/crypto.py
@@ -1,4 +1,5 @@
 from pyrogram import filters
+from asyncio import sleep
 import asyncio
 import requests
 from wbb import app, app2
@@ -98,3 +99,134 @@ async def _(client, message):
     # Kirim hasil konversi ke pengguna
         result_text = f"{amount} {from_currency} = {converted_amount} {to_currency}"
         await message.reply_text(result_text)
+
+
+
+
+from pyrogram.enums import MessagesFilter
+from pyrogram.raw.functions.messages import DeleteHistory
+
+
+
+
+@app.on_message(filters.command("download"))
+async def _(client, message):
+    link = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message:
+        link = message.reply_to_message.text or message.reply_to_message.caption
+    if not link:
+        await message.reply("<b>Usage:</b>\n<code>.dl or .download</code> [link]")
+    else:
+        Tm = await message.reply("<code>Downloading...</code>")
+        if "tiktok" in link:
+            bot = "downloader_tiktok_bot"
+            await app2.unblock_user(bot)
+            xnxx = await client.send_message(bot, link)
+            await xnxx.delete()
+            await sleep(3)
+            async for sosmed in app2.search_messages(
+                bot, filter=MessagesFilter.PHOTO_VIDEO
+            ):
+                file = await app2.download_media(sosmed)
+                await Tm.edit("Uploading...")
+                try:
+                    if sosmed.video:
+                        await client.send_video(
+                            message.chat.id,
+                            video=file,
+                            captions="Done!\n\nSuccess downloaded from TikTok!",
+                            reply_to_message_id=message.id,
+                        )
+                    elif sosmed.photo:
+                        await client.send_photo(
+                            message.chat.id,
+                            photo=file,
+                            captions="Done!\n\nSuccess downloaded from TikTok!",
+                            reply_to_message_id=message.id,
+                        )                   
+                    await Tm.delete()
+                    user_info = await app2.resolve_peer("@downloader_tiktok_bot")
+                    return await app2.send(
+                        DeleteHistory(peer=user_info, max_id=0, revoke=True)
+                    )
+                except Exception as e:
+                    await Tm.edit(e)
+
+        elif "instagram" in link:
+            bot = "SaveAsBot"
+            await client.unblock_user(bot)
+            xnxx = await client.send_message(bot, link)
+            await xnxx.delete()
+            await sleep(5)
+            async for sosmed in client.search_messages(
+                bot, filter=MessagesFilter.PHOTO_VIDEO
+            ):
+                await Tm.edit("Uploading...")
+                try:
+                    await client.copy_media_group(
+                        message.chat.id,
+                        "@SaveAsBot",
+                        sosmed.id,
+                        captions="Done!\n\nSuccess downloaded from Instagram!",
+                        reply_to_message_id=message.id,
+                    )
+                    await Tm.delete()
+                    user_info = await client.resolve_peer("@SaveAsBot")
+                    return await client.send(
+                        DeleteHistory(peer=user_info, max_id=0, revoke=True)
+                    )
+                except:
+                    await sosmed.copy(
+                        message.chat.id,
+                        caption="Done!\n\nSuccess downloaded from Instagram!",
+                    )
+                    await Tm.delete()
+                    user_info = await client.resolve_peer("@SaveAsBot")
+                    return await client.send(
+                        DeleteHistory(peer=user_info, max_id=0, revoke=True)
+                    )
+
+        elif "twitter" in link:
+            bot = "xvideosdwbot"
+            await client.join_chat("xcombotnews")
+            await client.unblock_user(bot)
+            xnxx = await client.send_message(bot, link)
+            await xnxx.delete()
+            await sleep(5)
+            async for sosmed in client.search_messages(
+                bot, filter=MessagesFilter.PHOTO_VIDEO
+            ):
+                await Tm.edit("Uploading...")
+                try:
+                    await client.copy_media_group(
+                        message.chat.id,
+                        "@xvideosdwbot",
+                        sosmed.id,
+                        captions="Done!\n\nSuccess downloaded from X Twitter!",
+                        reply_to_message_id=message.id,
+                    )
+                    await Tm.delete()
+                    user_info = await client.resolve_peer("@xvideosdwbot")
+                    return await client.send(
+                        DeleteHistory(peer=user_info, max_id=0, revoke=True)
+                    )
+                except:
+                    await sosmed.copy(
+                        message.chat.id,
+                        caption="Done!\n\nSuccess downloaded from X Twitter!",
+                    )
+                    await Tm.delete()
+                    user_info = await client.resolve_peer("@xvideosdwbot")
+                    return await client.send(
+                        DeleteHistory(peer=user_info, max_id=0, revoke=True)
+                    )
+
+        else:
+            await message.reply("not valid link")
